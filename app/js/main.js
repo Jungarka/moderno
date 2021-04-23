ab3ebaaf7e5618ec96bb7d36917b3b068d7dca89
Jungarka <k-butuzova@mail.ru>
2021-04-23 09:10:22 +0300

    added tutorial section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
+    $(".rate-star").rateYo({
+        rating: 4,
+        starWidth: "12px",
+        readOnly: true,
+    });
 
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true,
+         
+    });
+
+    var mixer = mixitup('.products__inner-box');
 });
